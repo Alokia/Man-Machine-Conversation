4429ecb0de0bf7586c477dee47ac4e97750b62e8
Eloik <443345680@qq.com>
2023-12-10 22:19:42 +0800

    record your voice

---
+++ utils/record.py
@@ -0,0 +1,48 @@
+import pyaudio
+import threading
+import wave
+
+
+class RecordAudio:
+    # 录自己的声音，保存为wav文件
+
+    def __init__(self, save_path="input.wav", chunk=1024, channels=1, rate=16000):
+        self.CHUNK = chunk
+        self.FORMAT = pyaudio.paInt16
+        self.CHANNELS = channels
+        self.RATE = rate
+        self._running = True
+        self._frames = []
+        self.save_path = save_path
+
+    def start(self):
+        threading._start_new_thread(self.__recording, ())
+
+    def __recording(self):
+        self._running = True
+        self._frames = []
+        p = pyaudio.PyAudio()
+        stream = p.open(format=self.FORMAT,
+                        channels=self.CHANNELS,
+                        rate=self.RATE,
+                        input=True,
+                        frames_per_buffer=self.CHUNK)
+        while self._running:
+            data = stream.read(self.CHUNK)
+            self._frames.append(data)
+
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+    def stop(self):
+        self._running = False
+
+    def save(self):
+        p = pyaudio.PyAudio()
+        wf = wave.open(self.save_path, 'wb')
+        wf.setnchannels(self.CHANNELS)
+        wf.setsampwidth(p.get_sample_size(self.FORMAT))
+        wf.setframerate(self.RATE)
+        wf.writeframes(b''.join(self._frames))
+        wf.close()
